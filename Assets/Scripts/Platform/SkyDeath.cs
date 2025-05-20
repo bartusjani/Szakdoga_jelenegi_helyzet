389efcf7e495546fe005d053fa8f155bdd9362dc
jani <bartusjani004@gmail.com>
2025-05-20 10:03:07 +0200

    backgrounds dirtier, player death if falls off

---
+++ Assets/Scripts/Platform/SkyDeath.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SkyDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.Die();
+            }
+        }
+    }
+}
