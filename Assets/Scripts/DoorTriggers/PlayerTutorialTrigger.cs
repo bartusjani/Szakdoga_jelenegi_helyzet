29137690ad164f9bf915a5d84515a252a5f00280
jani <bartusjani004@gmail.com>
2025-05-28 18:40:35 +0200

    move, combat tutorial UI and script changes

--- Assets/Scripts/DoorTriggers/PlayerTutorialTrigger.cs
+++ Assets/Scripts/DoorTriggers/PlayerTutorialTrigger.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTutorialTrigger : MonoBehaviour
@@ -6,6 +7,10 @@ public class PlayerTutorialTrigger : MonoBehaviour
     public GameObject combatHelp;
     public GameObject blockHelp;
     public GameObject platformerHelp;
+    public List<GameObject> tutorialSteps;
+    public List<GameObject> combatSteps;
+
+    int currentStep = 0;
     int helpcounter = 0;
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -13,28 +18,69 @@ public class PlayerTutorialTrigger : MonoBehaviour
         if (collision.CompareTag("PlatformCollider") && helpcounter<1)
         {
             //platform
+            helpcounter++;
             platformerHelp.SetActive(true);
-            StartCoroutine(SetsActiveFalse(platformerHelp));
+            Time.timeScale = 0;
         }
         else if (collision.CompareTag("SecondDoor") && helpcounter < 2)
         {
-            //platform
+            //combat
+            helpcounter++;
             combatHelp.SetActive(true);
-            StartCoroutine(SetsActiveFalse(combatHelp));
+            Time.timeScale = 0;
         }
         else if (collision.CompareTag("BlockCollider") && helpcounter < 3)
         {
-            //platform
+            //block
+            helpcounter++;
             blockHelp.SetActive(true);
-            StartCoroutine(SetsActiveFalse(blockHelp));
+            Time.timeScale = 0;
+        }
+    }
+    private void ShowStep(int index)
+    {
+        currentStep = index;
+        for (int i = 0; i < tutorialSteps.Count; i++)
+        {
+            tutorialSteps[i].SetActive(i == index);
         }
     }
 
-    IEnumerator SetsActiveFalse(GameObject gameObject)
+    public void NextTutorialStep()
     {
-        helpcounter++;
-        yield return new WaitForSeconds(5f);
+        if (currentStep < tutorialSteps.Count - 1)
+        {
+            tutorialSteps[currentStep].SetActive(false);
+            currentStep++;
+            tutorialSteps[currentStep].SetActive(true);
+        }
+    }
+
+    private void ShowCombatStep(int index)
+    {
+        currentStep = index;
+        for (int i = 0; i < combatSteps.Count; i++)
+        {
+            combatSteps[i].SetActive(i == index);
+        }
+    }
+
+    public void NextCombatStep()
+    {
+        if (currentStep < combatSteps.Count - 1)
+        {
+            combatSteps[currentStep].SetActive(false);
+            currentStep++;
+            combatSteps[currentStep].SetActive(true);
+        }
+    }
+
+    public void CloseTutorial(GameObject gameObject)
+    {
+
         gameObject.SetActive(false);
+        Time.timeScale = 1;
+
 
     }
 }
